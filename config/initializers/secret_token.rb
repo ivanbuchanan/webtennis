2bf8a89d4f689201d90252c4beaefe7ce0aa0052
Ivan Buchanan <buchanani2000@gmail.com>
2014-01-02 10:26:35 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Webtennis::Application.config.secret_key_base = 'da5d89c7cc9dbe634c28e1dc1adc7cdee402b72d13ede8fc54359004754effa72e811c0627dd23b29935bcb5d037d7a34329fab40789c38bbe4a037f2eedd7c6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Webtennis::Application.config.session_store :cookie_store, key: '_webtennis_session'
